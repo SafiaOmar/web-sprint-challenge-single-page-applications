19b33d06ee40484e8927fcc08293a94720aa1a55
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-11-12 13:15:30 -0500

    half  way there

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Form from "./Form";
 
 const App = () => {
   return (
-    <>
-      <h1> We put the P in Piźźà </h1>
-      <p> Safe space for crust lovers and crust haters</p>
-    </>
+    <div className="header">
+      <h1>Lambda Pizza We put the P in Piźźà</h1>
+      <p>Safe space for crust lovers and crust haters!</p>
+      <p>Disclaimer: All our pizza is made from vegan products</p>
+      <nav>
+      <Link to="/"><button id="home-button">Home</button></Link>
+      <Link to="/pizza"><button id="order-pizza">Order Pizza!</button></Link>
+      </nav>
+      <Switch>
+      <Route path="/pizza" component={Form} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </div>
   );
 };
 export default App;
